ea9fad6e4d4d3f0a4a66fcbec95a69eb5fd67c5b
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2024-09-06 09:26:09 +0200

    --wip-- [skip ci]

--- backend/lwca/resources/auth/register_resource.py
+++ backend/lwca/resources/auth/register_resource.py
@@ -8,7 +8,18 @@ register_parser.add_argument('email', type=str, required=True)
 register_parser.add_argument('password', type=str, required=True)
 register_parser.add_argument('confirm_password', type=str, required=True)
 
+register_resource_fields = {
+    'message': fields.String,
+    'access_token': fields.String,
+    'user': fields.Nested({
+        'id': fields.Integer,
+        'username': fields.String,
+        'email': fields.String
+    })
+}
+
 class RegisterResource(Resource):
+    @marshal_with(register_resource_fields)
     def post(self):
         args = register_parser.parse_args()
         message, error_code = handle_register_user(args)
