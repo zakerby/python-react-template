368602a784f754ececc044aca1f64f6871fe7c29
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2025-03-04 00:11:38 +0100

    Add dedicated yml files for swagger docs for user notifications

--- backend/lwca/routes/user_notifications.py
+++ backend/lwca/routes/user_notifications.py
@@ -7,48 +7,11 @@ blueprint = Blueprint('user_notifications', __name__)
 
 @blueprint.route('/api/v1/user/notifications', methods=['GET'])
 @jwt_required()
+@swag_from('../docs/user_notifications/get_user_notifications.yml')
 def get_user_notifications():
     """
     Get User Notifications endpoint
     ---
-    tags:
-      - Users
-    description: |
-      Allows a user to get their own notifications.
-      Requires a valid JWT token for authentication.
-    security:
-      - JWT: []
-    responses:
-      200:
-        description: User notifications retrieved successfully.
-        schema:
-          type: object
-          properties:
-            id:
-              type: integer
-              example: 1
-            title:
-              type: string
-              example: New Notification
-            date:
-              type: string
-              example: 2021-01-01T12:00:00
-            message:
-              type: string
-              example: This is a new notification
-            read:
-              type: boolean
-              example: false
-      401:
-        description: Unauthorized.  Missing or invalid JWT token.
-        schema:
-          type: object
-          properties:
-            message:
-              type: string
-              example: Missing Authorization Header
-      500:
-        description: Internal server error.
     """
     if request.method == 'GET':
         user_notifications, error_code = handle_get_user_notifications()
