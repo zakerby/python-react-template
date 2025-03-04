ea7049b1539fb6a84e586e301fd2b217d86f9b68
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2025-03-04 01:19:06 +0100

    Allow authorization bearer in swagger

--- backend/lwca/config/swagger.py
+++ backend/lwca/config/swagger.py
@@ -3,6 +3,14 @@ def get_swagger_config():
         Get the configuration for the Swagger UI
     """
     return {
+        "securityDefinitions": {
+            'bearerAuth': {
+                'type': 'apiKey',
+                'name': 'Authorization',
+                'in': 'header',
+                'description': 'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer {token}"'
+            },
+        },
         "headers": [
         ],
         "specs": [
