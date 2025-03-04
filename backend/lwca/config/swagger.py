464862ac66d6df39b224feef816fcf655637e1e0
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2025-03-04 12:57:36 +0100

    Fix wrong merge in swagger.py

--- backend/lwca/config/swagger.py
+++ backend/lwca/config/swagger.py
@@ -3,14 +3,6 @@ def get_swagger_config():
         Get the configuration for the Swagger UI
     """
     return {
-        "securityDefinitions": {
-            'bearerAuth': {
-                'type': 'apiKey',
-                'name': 'Authorization',
-                'in': 'header',
-                'description': 'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer {token}"'
-            },
-        },
         "headers": [
         ],
         "securityDefinitions": {
