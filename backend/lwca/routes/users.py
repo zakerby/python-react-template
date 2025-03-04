e83fb2e92ce8f2a964d3b51df6dab487c538084a
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2025-03-04 13:16:57 +0100

    Add get_user endpoint

--- backend/lwca/routes/users.py
+++ backend/lwca/routes/users.py
@@ -2,9 +2,21 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
 from flasgger import swag_from
 
-from lwca.handlers.users_handler import handle_delete_user
+from lwca.handlers.users_handler import handle_delete_user, handle_get_user
 
 blueprint = Blueprint('users', __name__)
+
+@blueprint.route('/api/v1/user', methods=['GET'])
+@jwt_required()
+@swag_from('../docs/users/get_user.yml')
+def get_user():
+  """
+  Get User endpoint
+  ---
+  """
+  if request.method == 'GET':
+      user, error_code = handle_get_user()
+      return jsonify(user), error_code
 
 @blueprint.route('/api/v1/user', methods=['DELETE'])
 @jwt_required()
